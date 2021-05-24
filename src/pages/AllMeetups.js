ebb2577b5f16924268c591a86fa3db8da672b65b
Shruti <shrutipulstya9@gmail.com>
2021-05-24 17:52:48 +0530

    added a fully functional form with db functionality

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,31 +1,46 @@
-import MeetupList from '../components/meetups/MeetupList';
+import { useState, useEffect } from "react";
 
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
+import MeetupList from "../components/meetups/MeetupList";
 
 function AllMeetupsPage() {
+  const [isLoading, setisLoading] = useState(true);
+  const [loadedMeetups, setloadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setisLoading(true);
+    fetch(
+      "https://react-meetup-app-7b20b-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+
+        setisLoading(false);
+        setloadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h1>All Meetups</h1>
-      <MeetupList meetups={DUMMY_DATA}/>
+      <MeetupList meetups={loadedMeetups} />
     </section>
   );
 }
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,31 @@
+import { useHistory } from "react-router-dom";
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-  return <section>
-    <h1>Add New Meetup</h1>
-  </section>;
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetup-app-7b20b-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
